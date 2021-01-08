f1e15ec4e1f801d52c335ed44135a181bc1efebb
paduhq <63902328+paduhq@users.noreply.github.com>
2021-01-08 08:30:07 +0800

    Update footer.js

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -5,18 +5,18 @@ function footer() {
     <div className="nav">
       <div className="content">
         <div className="nav-logo">
-          <h1 className="h4 m-0">Copyright @ 2020. <a className="alpha" href="https://github.com/shakilofficial0/Heroku-CyberTOR">CyberTOR-Beta</a></h1>
+          <h1 className="h4 m-0">Copyright @ 2020. <a className="alpha" href="https://t.me/PaduHQ">Padu HQ</a></h1>
         </div>
         <div className="nav-links" style={{flexDirection: "row"}}>
-          <a className="btn primary" href="https://www.facebook.com/shakilofficialdll" style={{color: "#f7fafc"}}>
+          <a className="btn primary" href="https://www.facebook.com/PaduHQ" style={{color: "#f7fafc"}}>
     <span className="bnt-icon">
         <ion-icon name="logo-facebook" />
       </span> Facebook
     </a>
-     <a className="btn" href="https://github.com/shakilofficial0" style={{color: "#f7fafc"}}>
+     <a className="btn" href="https://t.me/LutfiHamka" style={{color: "#f7fafc"}}>
     <span className="bnt-icon">
-        <ion-icon name="logo-github" />
-      </span> Github
+        <ion-icon name="logo-telegram" />
+      </span> Owner
     </a>
         </div>
       </div>
